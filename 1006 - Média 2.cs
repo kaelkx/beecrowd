3742fbe6dc7cdcf172b9ec84ed1dbbd19d3669ce
Pablo Kael <88331998+kaelkx@users.noreply.github.com>
2023-05-31 10:11:01 -0300

    1006 - Média 2

---
+++ 1006 - Média 2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Xml.Resolvers;
+
+namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var NotaA = double.Parse(Console.ReadLine());
+            var NotaB = double.Parse(Console.ReadLine());
+            var Notac = double.Parse(Console.ReadLine());
+
+            var MEDIA = (NotaA * 2 + NotaB * 3 + Notac * 5) / 100;
+
+            Console.WriteLine("MEDIA = " + MEDIA.ToString("F1", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}

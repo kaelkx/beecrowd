1f9104af6fdecbcf33457abcc4b70f97317b3026
Pablo Kael <88331998+kaelkx@users.noreply.github.com>
2023-05-31 12:30:48 -0300

    1008 - Salário

---
+++ 1008 - Salário.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace beecrowd
+{
+   public class Salario
+    {
+        public void getSalario ()
+        {
+
+
+            int NUMBER = int.Parse(Console.ReadLine());
+            int Horas_Trabalhadas = int.Parse(Console.ReadLine());
+            float ValorHora = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            float SALARY = Horas_Trabalhadas * ValorHora;
+
+            Console.WriteLine("NUMBER = " + NUMBER);
+            Console.WriteLine("SALARY = U$ " + SALARY.ToString("F2", System.Globalization.CultureInfo.InvariantCulture));
+
+
+
+        }
+    }
+}

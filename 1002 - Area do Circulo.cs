f58a4f8fd4c52434a4d18c43730a24234f9e1447
Pablo Kael <88331998+kaelkx@users.noreply.github.com>
2023-06-01 12:57:35 -0300

    1002 - Area do Circulo

---
+++ 1002 - Area do Circulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace beecrowd
+{
+    public class AreaDoCirculo
+    {
+        public void getArea()
+        {
+
+            double raio = double.Parse(Console.ReadLine());
+            double pi = 3.14159;
+
+            double area = pi * Math.Pow(raio, 2);
+
+            Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
+            
+            
+            //A = 12.5664
+        }
+    }
+}
+

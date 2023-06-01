fa172048d6b28aa62258e7dc0f7d754035cc85ae
Pablo Kael <88331998+kaelkx@users.noreply.github.com>
2023-06-01 11:38:18 -0300

    1001 - Extremamente Básico

---
+++ 1001 - Extremamente Básico.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace beecrowd
+{
+    public class Extremamente_Basico
+    {
+        public void getSoma()
+        {
+
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+
+
+
+            int X = A + B;
+
+            Console.WriteLine("X = " + X);
+
+
+
+        }
+    }
+}

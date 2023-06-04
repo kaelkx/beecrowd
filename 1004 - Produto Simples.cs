dcdaaf502fd22090a29587b7a8ece757332f8318
Pablo Kael <88331998+kaelkx@users.noreply.github.com>
2023-06-04 18:32:40 -0300

    1004 - Produto Simples

---
+++ 1004 - Produto Simples.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace beecrowd
+{
+    public class Produto_Simples
+    {
+        public void getProd()
+        {
+
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+
+            int PROD = A * B;
+
+            Console.WriteLine("PROD = " + PROD);
+
+
+        }
+
+
+    }
+
+}

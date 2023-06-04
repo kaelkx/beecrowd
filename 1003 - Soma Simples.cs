ddd6ab638d8438c36309048e776954e8da06d78e
Pablo Kael <88331998+kaelkx@users.noreply.github.com>
2023-06-04 18:17:49 -0300

    1003 - Soma Simples

---
+++ 1003 - Soma Simples.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace beecrowd
+{
+    public class Soma_Simples
+        {
+        static void Main(string[] args)
+        {
+
+            
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+
+            int SOMA = A + B;
+
+            Console.WriteLine("SOMA = " + SOMA);
+                
+        
+            }
+
+
+        }
+
+}

69a5a61012f84fdb7db47318dd5a09a70cfe1a13
Pablo Kael <88331998+kaelkx@users.noreply.github.com>
2023-06-06 20:31:58 -0300

    1014 - Consumo

---
+++ 1014 - Consumo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Xml.Resolvers;
+
+namespace PrimeiroProjeto
+{
+    public class Consumo
+    {
+        public void getConsumo()
+        {
+            int x = int.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+
+            double Consumo_Medio_de_Combustível = x / y;
+
+            Console.WriteLine(Consumo_Medio_de_Combustível.ToString("F3") + "km/l");
+        }
+    }
+}

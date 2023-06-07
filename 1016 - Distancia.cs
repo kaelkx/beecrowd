11055ffc7e82a3440b96e6eb4f33609d46238296
Pablo Kael <88331998+kaelkx@users.noreply.github.com>
2023-06-07 00:30:11 -0300

    1016 - Distancia

---
+++ 1016 - Distancia.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Xml.Resolvers;
+
+namespace PrimeiroProjeto
+{
+    public class Distancia
+    {
+        public void getDistancia()
+        {
+
+
+            int distancia = int.Parse(Console.ReadLine());
+
+            int velocidadeX = 60;
+            int velocidadeY = 90;
+
+            int velocidadeRelativa = velocidadeY - velocidadeX;
+
+            int tempo = distancia / velocidadeRelativa;
+
+            Console.WriteLine(tempo + " minutos");
+
+
+
+        }
+    }
+}
